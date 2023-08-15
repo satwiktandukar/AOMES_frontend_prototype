c783c3b4196894277819a556b157c54167f11265
Satwik Tandukar <84093132+satwiktandukar@users.noreply.github.com>
2023-08-15 23:40:58 +1000

    init 3

---
+++ lib/welcome.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+
+
+const kpink = Color.fromARGB(255, 116, 212, 107);
+const kblue = Color.fromARGB(193, 113, 255, 137);
+const kpurple = Color(0xFF9ba0fc);
+const korange = Color(0xFFffaa5b);
+const button = Color.fromARGB(255, 52, 148, 57);
+const nav = Color.fromARGB(255, 92, 209, 111);
